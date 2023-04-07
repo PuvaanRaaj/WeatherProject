09266c82c9b06408a7b46d34b4a58f98b8c8a96e
PuvaanRaaj <112681813+PuvaanRaaj@users.noreply.github.com>
2023-04-08 00:43:03 +0800

    added files

--- app.js
+++ app.js
@@ -128,6 +128,7 @@ app.post("/", (req, res) => {
 });
 
 // Port: 3000
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
 });
